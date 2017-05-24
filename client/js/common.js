cb2646ad5260b3b6ecde2353862d48e85f5a9475
SergeyLys <s.lysykh@makebecool.com>
2017-05-24 22:02:30 +0300

    updates

--- client/js/common.js
+++ client/js/common.js
@@ -1,13 +1,40 @@
 import '../css/styles.scss';
 
 class Common {
-    constructor() {
-        this.headerFunctions();
-    }
+	constructor(prop) {
+		this.header = header;
+	}
 
-    headerFunctions() {
-        console.log('Init common header when DOM has loaded');
-    }
+	openMenu() {
+		console.log(this.header, 'open menu');
+	}
 }
 
-document.addEventListener('DOMContentLoaded', new Common());
+class Header {
+	constructor(header) {
+		this.header = header;
+		this.openMenu();
+	}
+
+	openMenu() {
+		document.addEventListener('click', function() {
+			console.log(document.getElementById('header'), 'open menu');
+		});
+	}
+}
+
+class StickyHeader extends Common {
+	constructor() {
+		super(document.getElementById('header'));
+		this.method1();
+	}
+
+	method1() {
+		console.log(this.header);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', new Sticky());
+document.addEventListener('scroll', function() {
+	new StickyHeader();
+});
